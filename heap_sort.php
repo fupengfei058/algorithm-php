d7216ceb7f54d4a37fa48dab9c9b58e15f029e9a
fupengfei058 <fupengfei@yaochufa.com>
2017-09-26 21:23:50 +0800

    9/26

---
+++ heap_sort.php
@@ -0,0 +1,58 @@
+<?php
+
+function exchange(&$a,&$b){
+        $temp = $b;
+        $b = $a;
+        $a = $temp;
+}
+
+function left($i){ return $i*2+1;}
+
+function right($i){ return $i*2+2;}
+
+function buildHeap(&$array,$i,$heapsize){
+
+        $left = left($i);
+        $right = right($i);
+        $max = $i;
+        if($i < $heapsize && $left<$heapsize  && $array[$left] > $array[$i] ){
+                $max = $left;
+        }
+
+        if($i < $heapsize && $right<$heapsize && $array[$right] > $array[$max]){
+                $max = $right;
+        }
+        if($i != $max && $i < $heapsize && $max < $heapsize){
+
+                exchange($array[$i],$array[$max]);
+                buildHeap($array,$max,$heapsize);
+
+        }
+}
+
+function sortHeap(&$array,$heapsize){
+        while($heapsize){
+                exchange($array[0],$array[$heapsize-1]);
+                $heapsize = $heapsize -1;
+                buildHeap($array,0,$heapsize);
+        }
+}
+
+function createHeap(&$array,$heapsize){
+        $i = ceil($heapsize/2)-1;
+        for(;$i>=0;$i--){
+                buildHeap($array,$i,$heapsize);
+        }
+}
+
+function main(){
+        $array = array(88,99,22,11,22,13,9,2,1,100,12);
+        $heapsize = count($array);
+        createHeap($array,$heapsize);
+
+        print_r($array);
+        sortHeap($array,$heapsize);
+        print_r($array);
+}
+
+main();
