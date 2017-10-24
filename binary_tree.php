8113ea72f136f17b4ae25b9c13994c27fc087de0
fupengfei058 <fupengfei@yaochufa.com>
2017-10-24 23:56:41 +0800

    10/24

---
+++ binary_tree.php
@@ -0,0 +1,117 @@
+<?php
+
+class Node{
+    public $key,$left,$right;
+    public function __construct($key)
+    {
+        $this->key = $key;
+    }
+}
+
+class BinaryTree{
+    public $root;
+    public $sortArr = [];
+    // 插入节点
+    public function insertNode($node,$newNode){
+        if ($node->key < $newNode->key){
+            // 如果父节点小于子节点,插到右边
+            if (empty($node->right)){
+                $node->right = $newNode;
+            }else{
+                $this->insertNode($node->right,$newNode);
+            }
+        }elseif ($node->key > $newNode->key){
+            // 如果父节点大于子节点,插到左边
+            if (empty($node->left)){
+                $node->left = $newNode;
+            }else{
+                $this->insertNode($node->left,$newNode);
+            }
+        }
+    }
+    public function insert($key){
+        $newNode = new Node($key);
+        if (empty($this->root)){
+            $this->root = $newNode;
+        }else{
+            $this->insertNode($this->root,$newNode);
+        }
+    }
+    // 中序遍历
+    public function midSort(){
+        $this->midSortNode($this->root);
+    }
+    public function midSortNode($node){
+        if (!empty($node)){
+            $this->midSortNode($node->left);
+            array_push($this->sortArr,$node->key);
+            $this->midSortNode($node->right);
+        }
+    }
+    // 寻找极值
+    public function findMin(){
+        //不断的找它的左子树,直到这个左子树的节点为叶子节点.
+        if (!empty($this->root)){
+            $this->findMinNode($this->root);
+        }
+    }
+    public function findMinNode(Node $node){
+        if (!empty($node->left)){
+            $this->findMinNode($node->left);
+        }else{
+            echo '这个二叉树的最小值为:'.$node->key;
+        }
+    }
+    public function findMax(){
+        if (!empty($this->root)){
+            $this->findMaxNode($this->root);
+        }
+    }
+    public function findMaxNode(Node $node){
+        if (!empty($node->right)){
+            $this->findMaxNode($node->right);
+        }else{
+            echo '这个二叉树的最大值为:'.$node->key;
+        }
+    }
+    // 查找特定的值
+    public function find($val = ''){
+        if (!empty($val)){
+            $this->findNode($this->root,$val);
+        }
+    }
+    public function findNode(Node $node,$val){
+        if ($node->key == $val){
+            echo '找到'.$val.'了';
+        }else if ($node->key > $val){
+            // 如果 父节点的值 大于要查找的值,那么查找它的左子树
+            if (!empty($node->left)){
+                $this->findNode($node->left,$val);
+            }else{
+                echo '没有这个东西!';
+            }
+        }else if ($node->key < $val){
+            if (!empty($node->right)){
+                $this->findNode($node->right,$val);
+            }else{
+                echo '没有这个东西!';
+            }
+        }
+    }
+}
+
+$tree = new BinaryTree();
+// 节点插入
+$nodes = array(8,3,10,1,6,14,4,7,13);
+foreach ($nodes as $value){
+    $tree->insert($value);
+}
+// 中序遍历
+//$tree->midSort();
+//print_r($tree->sortArr);
+// 寻找极值
+//$tree->findMin();
+//$tree->findMax();
+// 查找特定的值
+$tree->find(7);
+$tree->find(11);
