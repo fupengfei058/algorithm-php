e876b5f7fb8df6e17cdf40de55ef51b712e877c7
fupengfei058 <fupengfei@yaochufa.com>
2017-11-12 11:50:41 +0800

    11/12

--- binary_tree.php
+++ binary_tree.php
@@ -101,7 +101,61 @@ class BinaryTree{
         }
     }
 
-    //删除最小节点
+    //先序遍历 根节点 ---> 左子树 ---> 右子树
+    function preorder($root){
+        $stack=array();
+        array_push($stack,$root);
+        while(!empty($stack)){
+            $center_node=array_pop($stack);
+            echo $center_node->value.' ';//先输出根节点
+            if($center_node->right!=null){
+                array_push($stack,$center_node->right);
+            }
+            if($center_node->left!=null){
+                array_push($stack,$center_node->left);
+            }
+        }
+    }
+    //中序遍历，左子树---> 根节点 ---> 右子树
+    function inorder($root){
+        $stack = array();
+        $center_node = $root;
+        while (!empty($stack) || $center_node != null) {
+            while ($center_node != null) {
+                array_push($stack, $center_node);
+                $center_node = $center_node->left;
+            }
+
+            $center_node = array_pop($stack);
+            echo $center_node->value . " ";
+
+            $center_node = $center_node->right;
+        }
+    }
+    //后序遍历，左子树 ---> 右子树 ---> 根节点
+    function tailorder($root)
+    {
+        $stack = array();
+        $outstack = array();
+        array_push($stack, $root);
+        while (!empty($stack)) {
+            $center_node = array_pop($stack);
+            array_push($outstack, $center_node);//最先压入根节点，最后输出
+            if ($center_node->left != null) {
+                array_push($stack, $center_node->left);
+            }
+            if ($center_node->right != null) {
+                array_push($stack, $center_node->right);
+            }
+        }
+
+        while (!empty($outstack)) {
+            $center_node = array_pop($outstack);
+            echo $center_node->value . ' ';
+        }
+    }
+
+      //删除最小节点
 //    public function deleteMin(){
 //        $this->findMinNode($this->root);
 //    }
